ed5595c5c64817da698f34535ebe888ed9dca9c7
Enzo Conejero <enzocone13@gmail.com>
2018-10-03 12:52:03 -0300

    Verifica que el json tenga los campos

---
+++ src/main/java/conexion_http/Cliente.java
@@ -0,0 +1,23 @@
+package conexion_http;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class Cliente {
+	private Client cliente;
+	private WebResource webResource;
+	private static final String resource = "http://notitas.herokuapp.com/";
+	private static final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTEyMjIzMzMiLCJybmQiOiJ5SXNmZFIwN2lIR3BRRmVjYU9KT2VRPT0ifQ.9pVJGUXhrJPQ-TptNCt971l0h_1dWqWgMrHAWXJchho";
+	
+	public Cliente() {
+		this.cliente = Client.create();
+	}
+	
+	public ClientResponse getEstudiante() {
+		webResource = this.cliente.resource(Cliente.resource + "student");
+		return webResource.header("Authorization","Bearer " + Cliente.token)
+				.accept("application/json").get(ClientResponse.class); 
+	}
+	
+}
--- src/test/java/test_conexion/ContextTest.java
+++ src/test/java/test_conexion/ContextTest.java
@@ -1,27 +1,23 @@
 package test_conexion;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.*;
+import conexion_http.Cliente;
 
 public class ContextTest {
 
+
 	@Test
-	public void contextUp() {
-		Client client = Client.create();
-
-		WebResource webResource = client.resource("http://notitas.herokuapp.com/student");
-
-		ClientResponse response = webResource.header("Authorization",
-				"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMTEyMjIzMzMiLCJybmQiOiJ5SXNmZFIwN2lIR3BRRmVjYU9KT2VRPT0ifQ.9pVJGUXhrJPQ-TptNCt971l0h_1dWqWgMrHAWXJchho")
-				.accept("application/json").get(ClientResponse.class);
+	public void context() {
+		Cliente cliente = new Cliente();
+		ClientResponse response = cliente.getEstudiante();
 		assertEquals(200, response.getStatus());
+        String json = response.getEntity(String.class);
+        assertTrue(json.contains("code"));
+        assertTrue(json.contains("first_name"));
+        assertTrue(json.contains("last_name"));
+        assertTrue(json.contains("github_user"));
 	}
-
 	
-
 }
